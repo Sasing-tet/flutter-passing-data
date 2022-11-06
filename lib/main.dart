82944fe1ce7bc0ae40d8a0d50cf45189d26a2d46
Vence Jumar Sasing <rjsasing@gmail.com>
2022-11-06 17:32:09 +0800

    passing data to another widget

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, unnecessary_new, prefer_const_constructors_in_immutables, unnecessary_string_interpolations
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  var _textController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: TextField(
+              controller: _textController,
+            ),
+          ),
+          ListTile(
+            title: ElevatedButton(
+              child: Text("Send"),
+              onPressed: () {
+                var route = MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      NextPage(value: _textController.text),
+                );
+                Navigator.of(context).push(route);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NextPage extends StatefulWidget {
+  final String value;
+
+  NextPage({Key? key, required this.value}) : super(key: key);
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("2nd Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          "${widget.value}",
+          style: TextStyle(
+            fontSize: 20
+          ),
+          ),
+      ),
+    );
+  }
+}
